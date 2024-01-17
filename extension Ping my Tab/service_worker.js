2e77a94c6bdd3a6cda07bca6d55c2fd824fca436
Amy <yambeddm@gmail.com>
2024-01-17 14:42:10 +0100

    notifications ajoutées

--- extension Ping my Tab/service_worker.js
+++ extension Ping my Tab/service_worker.js
@@ -4,23 +4,46 @@ function updateIcon() {
   chrome.tabs.query({}, function (tabs) {
     var tabCount = tabs.length;
     var iconPath;
+    var iconUrl;
+    var iconTitle;
+    var iconMessage;
     // set icon's color depending on number of tabs
     if (tabCount < 6) {
       iconPath = 'icons/greenlight.png';
+      iconUrl = 'icons/icons8-sun-32.png';
+      iconTitle = 'Tabs alert'
+      iconMessage = 'Good job'
     } else if (tabCount < 12) {
       iconPath = 'icons/orangelight.png';
+      iconUrl = 'icons/icons8-cloud-32.png';
+      iconTitle = 'Tabs alert';
+      iconMessage = 'Slow down';
     } else {
       iconPath = 'icons/redlight.png';
+      iconUrl = 'icons/icons8-storm-32.png';
+      iconTitle = 'Tabs alert';
+      iconMessage = 'Abort mission';
     }
     // set icon's image path
     chrome.action.setIcon({ path: iconPath });
-  });
-}
+ 
 
+  if (iconPath === 'icons/greenlight.png' || iconPath === 'icons/redlight.png' || iconPath === 'icons/orangelight.png'){
+    const notificationOptions = {
+      type: 'basic',
+      iconUrl: iconUrl,
+      title: iconTitle,
+      message: iconMessage
+    }
+ 
+    chrome.notifications.create(notificationOptions)
+  };
+})
+}
 // Initial icon update
 updateIcon();
 
 // Listen for tab creation and removal events
-  chrome.tabs.onCreated.addListener(updateIcon);
+chrome.tabs.onCreated.addListener(updateIcon);
 
-  chrome.tabs.onRemoved.addListener(updateIcon);
+chrome.tabs.onRemoved.addListener(updateIcon);
